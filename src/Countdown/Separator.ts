22a07cd4f092caaf95f9b1e4415edf8d20e47614
EvvTim <evgeniytim94@gmail.com>
2021-11-18 21:29:09 +0100

    Updates

--- src/Countdown/Separator.ts
+++
@@ -1,9 +0,0 @@
-import Control from "../Control";
-
-class Separator extends Control {
-  constructor(parentNode: HTMLElement) {
-    super(parentNode, "span", "countdown-separator", ":");
-  }
-}
-
-export default Separator;
--- src/index.ts
+++
@@ -1,6 +0,0 @@
-import App from "./App";
-
-window.onload = () => {
-  const app = new App(document.body);
-  return app;
-};
